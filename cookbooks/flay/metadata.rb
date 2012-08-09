524285d73c07a90a27d719d7417e32a921822478
Michael <lxit@lucernex.com>
2012-08-09 15:34:55 +0000

    Added rightscale_marker to default.rb

--- cookbooks/flay/metadata.rb
+++ cookbooks/flay/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures flay"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "flay::default","Prints hello world output"
--- cookbooks/flay/recipes/default.rb
+++ cookbooks/flay/recipes/default.rb
@@ -7,4 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
+rightscale_marker :begin
 log "Hello World! Rightscale rocks!"
+rightscale_marker :end
